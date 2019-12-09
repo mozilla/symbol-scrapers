30334616f4c9696278dff18d3543456d862003cb
Gabriele Svelto <gsvelto@mozilla.com>
2019-12-09 11:35:29 +0100

    Added Firefox developer edition & cairo to the packages scraped from Arch

--- arch/script.sh
+++ arch/script.sh
@@ -36,8 +36,10 @@ mkdir -p tmp
 
 cd tarballs
 packages="
+https://www.archlinux.org/packages/community/x86_64/firefox-developer-edition/download/
 https://www.archlinux.org/packages/core/x86_64/glib2/download/
 https://www.archlinux.org/packages/core/x86_64/glibc/download/
+https://www.archlinux.org/packages/extra/x86_64/cairo/download/
 https://www.archlinux.org/packages/extra/x86_64/dconf/download/
 https://www.archlinux.org/packages/extra/x86_64/gtk3/download/
 https://www.archlinux.org/packages/extra/x86_64/libpulse/download/
@@ -103,8 +105,10 @@ find symbols -mindepth 2 -maxdepth 2 -type d | while read module; do
   fi
 done
 
+purge "firefox-developer-edition-" "https://ind.mirror.pkgbuild.com/community/os/x86_64/"
 purge "glib2-" "http://mirror.f4st.host/archlinux/core/os/x86_64/"
 purge "glibc-" "http://mirror.f4st.host/archlinux/core/os/x86_64/"
+purge "cairo-" "http://mirror.f4st.host/archlinux/extra/os/x86_64/"
 purge "dconf-" "http://mirror.f4st.host/archlinux/extra/os/x86_64/"
 purge "gtk3-" "http://mirror.f4st.host/archlinux/extra/os/x86_64/"
 purge "libpulse-" "http://mirror.f4st.host/archlinux/extra/os/x86_64/"
