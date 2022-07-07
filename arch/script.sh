c7a0e7efb6518b8cc8412ff4f2a9f5559254c989
Gabriele Svelto <gsvelto@mozilla.com>
2022-07-07 15:50:25 +0200

    Added support for debuginfo packages on Arch

--- arch/script.sh
+++ arch/script.sh
@@ -2,6 +2,91 @@
 
 . $(dirname $0)/../common.sh
 
+URL="https://geo.mirror.pkgbuild.com"
+
+REPOS="
+community/os/x86_64
+community-debug/os/x86_64
+core/os/x86_64
+core-debug/os/x86_64
+extra/os/x86_64
+extra-debug/os/x86_64
+"
+
+function get_package_urls() {
+  local package_name="${1}"
+  local dbg_package_name="${package_name}-debug"
+  local url=${2:-$URL}
+
+  find . -name "index.html*" -exec grep -o "${url}.*/\(${package_name}-[0-9].*-x86_64.pkg.tar.zst\|${dbg_package_name}-[0-9].*-x86_64.pkg.tar.zst\)\"" {} \; | \
+  cut -d'"' -f1
+}
+
+function get_package_indexes() {
+  echo "${REPOS}" | while read line; do
+    [ -z "${line}" ] && continue
+    printf "${URL}/${line}/\n"
+  done | sort -u > indexes.txt
+}
+
+function fetch_packages() {
+  get_package_indexes
+
+  wget -o wget_packages_urls.log --progress=dot:mega --compression=auto -k -i indexes.txt
+
+  find . -name "index.html*" | while read path; do
+    mv "${path}" "${path}.bak"
+    xmllint --nowarning --format --html --output "${path}" "${path}.bak" 2>/dev/null
+    rm -f "${path}.bak"
+  done
+
+  echo "${1}" | while read line; do
+    [ -z "${line}" ] && continue
+    get_package_urls ${line} >> packages.txt
+  done
+
+  find . -name "index.html*" -exec rm -f {} \;
+
+  wget -o wget_packages.log --progress=dot:mega -P downloads -c -i packages.txt
+
+  rev packages.txt | cut -d'/' -f1 | rev > package_names.txt
+}
+
+function get_version() {
+  package_name="${1}"
+  filename="${2}"
+
+  version="${filename##${package_name}-}"
+  version="${version%%.pkg.tar.zst}"
+  printf "${version}"
+}
+
+function find_debuginfo_package() {
+  package_name="${1}"
+  version="${2}"
+  find downloads -name "${package_name}-debug-${version}.pkg.tar.zst" -type f
+}
+
+function unpack_package() {
+  local package_path="${1}"
+  local debug_package_path="${2}"
+
+  mkdir packages debug-packages
+  tar -C packages -x -a -f "${package_path}"
+
+  if [ -n "${debug_package_path}" ]; then
+    tar -C debug-packages -x -a -f "${debug_package_path}"
+  fi
+}
+
+function get_build_id {
+  eu-readelf -n "${1}" | grep "^    Build ID:" | cut -b15-
+}
+
+function find_debuginfo() {
+  find debug-packages -path "*${1}.debug" -type f
+}
+
 function get_soname {
   local path="${1}"
   local soname=$(objdump -p "${path}" | grep "^  SONAME *" | cut -b24-)
@@ -10,13 +95,67 @@ function get_soname {
   fi
 }
 
+function remove_temp_files() {
+  rm -rf symbols packages debug-packages tmp symbols*.zip packages.txt package_names.txt
+}
+
 function generate_fake_packages() {
   cat SHA256SUMS | while read line; do
     local package_name=$(echo ${line} | cut -d',' -f1)
     local package_size=$(echo ${line} | cut -d',' -f2)
-    truncate --size "${package_size}" "tarballs/${package_name}"
+    truncate --size "${package_size}" "downloads/${package_name}"
   done
 }
+
+remove_temp_files
+mkdir -p downloads symbols tmp
+generate_fake_packages
+
+packages="
+amdvlk
+atk
+at-spi2-atk
+at-spi2-core
+cairo
+dbus
+dbus-glib
+dconf
+ffmpeg
+gcc-libs
+gdk-pixbuf2
+glib2
+glibc
+gtk3
+gvfs
+intel-media-driver
+libdrm
+libffi
+libglvnd
+libibus
+libice
+libpulse
+libsm
+libspeechd
+libstdc++5
+libva
+libva-mesa-driver
+libva-vdpau-driver
+libx11
+libxcb
+libxext
+libxkbcommon
+mesa
+nspr
+numactl
+nvidia-utils
+pcre2
+pixman
+vulkan-intel
+vulkan-radeon
+wayland
+"
+
+fetch_packages "${packages}"
 
 function add_package_to_list() {
   local package_filename=$(basename "${1}")
@@ -24,89 +163,91 @@ function add_package_to_list() {
   printf "${package_filename},${package_size}\n" >> SHA256SUMS
   truncate --size 0 "${1}"
   truncate --size "${package_size}" "${1}"
+
+  if [ -n "${2}" ]; then
+    local debuginfo_package_filename=$(basename "${2}")
+    local debuginfo_package_size=$(stat -c"%s" "${2}")
+    printf "${debuginfo_package_filename},${debuginfo_package_size}\n" >> SHA256SUMS
+    truncate --size 0 "${2}"
+    truncate --size "${debuginfo_package_size}" "${2}"
+  fi
 }
 
-rm -rf symbols* tmp
-mkdir -p symbols tarballs tmp
-generate_fake_packages
+function process_packages() {
+  local package_name="${1}"
+  find downloads -name "${package_name}-[0-9]*.pkg.tar.zst" -type f | while read package; do
+    local package_filename="${package##downloads/}"
+    if ! grep -q -F "${package_filename}" SHA256SUMS; then
+      local version=$(get_version "${package_name}" "${package_filename}")
+      local debuginfo_package=$(find_debuginfo_package "${package_name}" "${version}")
 
-cd tarballs
-packages="
-https://archlinux.org/packages/community/x86_64/intel-media-driver/download/
-https://archlinux.org/packages/core/x86_64/dbus/download/
-https://archlinux.org/packages/core/x86_64/gcc-libs/download/
-https://archlinux.org/packages/core/x86_64/glib2/download/
-https://archlinux.org/packages/core/x86_64/glibc/download/
-https://archlinux.org/packages/core/x86_64/libffi/download/
-https://archlinux.org/packages/core/x86_64/nspr/download/
-https://archlinux.org/packages/extra/x86_64/amdvlk/download/
-https://archlinux.org/packages/extra/x86_64/atk/download/
-https://archlinux.org/packages/extra/x86_64/at-spi2-atk/download/
-https://archlinux.org/packages/extra/x86_64/at-spi2-core/download/
-https://archlinux.org/packages/extra/x86_64/cairo/download/
-https://archlinux.org/packages/extra/x86_64/dbus-glib/download/
-https://archlinux.org/packages/extra/x86_64/dconf/download/
-https://archlinux.org/packages/extra/x86_64/ffmpeg/download/
-https://archlinux.org/packages/extra/x86_64/gdk-pixbuf2/download/
-https://archlinux.org/packages/extra/x86_64/gtk3/download/
-https://archlinux.org/packages/extra/x86_64/gvfs/download/
-https://archlinux.org/packages/extra/x86_64/libdrm/download/
-https://archlinux.org/packages/extra/x86_64/libglvnd/download/
-https://archlinux.org/packages/extra/x86_64/libibus/download/
-https://archlinux.org/packages/extra/x86_64/libice/download/
-https://archlinux.org/packages/extra/x86_64/libpulse/download/
-https://archlinux.org/packages/extra/x86_64/libsm/download/
-https://archlinux.org/packages/extra/x86_64/libspeechd/download/
-https://archlinux.org/packages/extra/x86_64/libstdc++5/download/
-https://archlinux.org/packages/extra/x86_64/libva/download/
-https://archlinux.org/packages/extra/x86_64/libva-mesa-driver/download/
-https://archlinux.org/packages/extra/x86_64/libva-vdpau-driver/download/
-https://archlinux.org/packages/extra/x86_64/libx11/download/
-https://archlinux.org/packages/extra/x86_64/libxcb/download/
-https://archlinux.org/packages/extra/x86_64/libxext/download/
-https://archlinux.org/packages/extra/x86_64/libxkbcommon/download/
-https://archlinux.org/packages/extra/x86_64/mesa/download/
-https://archlinux.org/packages/extra/x86_64/numactl/download/
-https://archlinux.org/packages/extra/x86_64/nvidia-utils/download/
-https://archlinux.org/packages/extra/x86_64/pcre2/download/
-https://archlinux.org/packages/extra/x86_64/pixman/download/
-https://archlinux.org/packages/extra/x86_64/vulkan-intel/download/
-https://archlinux.org/packages/extra/x86_64/vulkan-radeon/download/
-https://archlinux.org/packages/extra/x86_64/wayland/download/
-"
-wget -o ../wget.log --progress=dot:mega -c --content-disposition ${packages}
-tarballs=$(ls)
-cd ..
+      truncate --size=0 error.log
 
-find tarballs -type f | while read path; do
-  tarball_filename=$(basename ${path})
-  if ! grep -q -F "${tarball_filename}" SHA256SUMS; then
-    tar -C tmp -x -a -f "${path}"
-    add_package_to_list "${path}"
-  fi
-done
-
-find tmp -name "*.so*" -type f | while read library; do
-  if file "${library}" | grep -q "ELF 64-bit LSB shared object" ; then
-    library_basename=$(basename "${library}")
-    debugid=$("${DUMP_SYMS}" "${library}" | head -n 1 | cut -b 21-53)
-    filename=$(basename "${library}")
-    mkdir -p "symbols/${filename}/${debugid}"
-    "${DUMP_SYMS}" "${library}" > "symbols/${filename}/${debugid}/${filename}.sym"
-    soname=$(get_soname "${library}")
-    if [ -n "${soname}" ]; then
-      if [ "${soname}" != "${filename}" ]; then
-        mkdir -p "symbols/${soname}/${debugid}"
-        cp "symbols/${filename}/${debugid}/${filename}.sym" "symbols/${soname}/${debugid}/${soname}.sym"
+      if [ -n "${debuginfo_package}" ]; then
+        unpack_package "${package}" "${debuginfo_package}"
+      else
+        printf "***** Could not find debuginfo for ${package_filename}\n"
+        unpack_package "${package}"
       fi
-    fi
-  fi
-done
 
-rm -rf tmp
+      find packages -type f | while read path; do
+        if file "${path}" | grep -q ": *ELF" ; then
+          local debuginfo_path="$(find_debuginfo "${path##packages}")"
+
+          local tmpfile=$(mktemp --tmpdir=tmp)
+          printf "Writing symbol file for ${path} ${debuginfo_path} ... "
+          if [ -n "${debuginfo_path}" ]; then
+            ${DUMP_SYMS} --type elf "${path}" "${debuginfo_path}" 1> "${tmpfile}" 2> error.log
+          else
+            ${DUMP_SYMS} --type elf "${path}" 1> "${tmpfile}" 2> error.log
+          fi
+
+          if [ -s "${tmpfile}" -a -z "${debuginfo_path}" ]; then
+            printf "done w/o debuginfo\n"
+          elif [ -s "${tmpfile}" ]; then
+            printf "done\n"
+          else
+            printf "something went terribly wrong!\n"
+          fi
+
+          if [ -s error.log ]; then
+            printf "***** error log for package ${package} ${path} ${debuginfo_path}\n"
+            cat error.log
+            printf "***** error log for package ${package} ${path} ${debuginfo_path} ends here\n"
+          fi
+
+          # Copy the symbol file
+          debugid=$(head -n 1 "${tmpfile}" | cut -d' ' -f4)
+          filename="$(basename "${path}")"
+          mkdir -p "symbols/${filename}/${debugid}"
+          cp "${tmpfile}" "symbols/${filename}/${debugid}/${filename}.sym"
+          local soname=$(get_soname "${path}")
+          if [ -n "${soname}" ]; then
+            if [ "${soname}" != "${filename}" ]; then
+              mkdir -p "symbols/${soname}/${debugid}"
+              cp "${tmpfile}" "symbols/${soname}/${debugid}/${soname}.sym"
+            fi
+          fi
+
+          rm -f "${tmpfile}"
+        fi
+      done
+
+      rm -rf packages debug-packages
+      add_package_to_list "${package}" "${debuginfo_package}"
+    fi
+  done
+}
+
+echo "${packages}" | while read line; do
+  [ -z "${line}" ] && continue
+  process_packages ${line}
+done
 
 zip_symbols
 
 upload_symbols
 
 reprocess_crashes
+
+remove_temp_files
