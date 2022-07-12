391242001b91eed53a1fdf898cbda2effb9f5b4b
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2022-07-12 10:23:12 +0200

    Bug 1778359 - Add GNOME SDK Snap pulling

---
+++ gnome-sdk-snap/script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. $(dirname $0)/../common.sh
+. $(dirname $0)/../launchpad.sh
+
+process_snap "gnome-3-38-2004" "desktop-snappers"
+
+zip_symbols
+
+upload_symbols
+
+reprocess_crashes
+
+remove_temp_files
