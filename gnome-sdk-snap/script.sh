a5ce2f400013094897fe83b9f77744198bd1fad2
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2022-08-02 16:37:02 +0200

    Bug 1782737 - Use correct store_name for GNOME SDK

--- gnome-sdk-snap/script.sh
+++ gnome-sdk-snap/script.sh
@@ -3,7 +3,7 @@
 . $(dirname $0)/../common.sh
 . $(dirname $0)/../launchpad.sh
 
-process_snap "gnome-3-38-2004" "desktop-snappers"
+process_snap "gnome-3-38-2004-sdk" "desktop-snappers"
 
 zip_symbols
 
