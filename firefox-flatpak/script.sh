b04f06a3a90c9995241feefba78ccf343633369a
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2023-06-08 11:58:37 +0200

    Add Firefox Flatpak processing

---
+++ firefox-flatpak/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. $(dirname $0)/../common.sh
+. $(dirname $0)/../flatpak.sh
+
+process_flatpak \
+	"org.mozilla.firefox/x86_64/stable" \
+	"org.freedesktop.Platform.GL.Debug.default/x86_64/22.08" \
+	"org.freedesktop.Platform.GL.Debug.default/x86_64/22.08-extra"
+
+zip_symbols
+
+upload_symbols
+
+reprocess_crashes
+
+remove_temp_files
