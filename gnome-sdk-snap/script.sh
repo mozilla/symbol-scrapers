7f235f0627bb9896d5271f87ab69179b2862598e
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2023-06-19 17:33:26 +0200

    Bug 1839171 - Add GNOME SDK 42 Snap

--- gnome-sdk-snap/script.sh
+++ gnome-sdk-snap/script.sh
@@ -3,7 +3,8 @@
 . $(dirname $0)/../common.sh
 . $(dirname $0)/../launchpad.sh
 
-process_snap "gnome-3-38-2004-sdk" "desktop-snappers"
+process_snap "gnome-3-38-2004-sdk" "gnome-3-38-2004-sdk" "desktop-snappers"
+process_snap "gnome-42-2204-sdk" "gnome-42-2204-sdk" "desktop-snappers"
 
 zip_symbols
 
