f2acecbec21fb21e7dcb9993478a535b26d58fa3
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2024-10-07 11:41:48 +0200

    Add GNOME 46 SDK scrapping

--- gnome-sdk-snap/script.sh
+++ gnome-sdk-snap/script.sh
@@ -5,8 +5,9 @@ export DEBUGINFOD_URLS="https://debuginfod.ubuntu.com/"
 . $(dirname $0)/../common.sh
 . $(dirname $0)/../launchpad.sh
 
-process_snap "gnome-3-38-2004-sdk" "gnome-3-38-2004-sdk" "desktop-snappers"
+#process_snap "gnome-3-38-2004-sdk" "gnome-3-38-2004-sdk" "desktop-snappers"
 process_snap "gnome-42-2204-sdk" "gnome-42-2204-sdk" "desktop-snappers"
+process_snap "gnome-46-2404-sdk" "gnome-46-2404-sdk" "desktop-snappers"
 
 create_symbols_archive
 
