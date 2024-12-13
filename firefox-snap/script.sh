8755148a5471e321f583429b0028391f2e336249
lissyx <1645737+lissyx@users.noreply.github.com>
2024-12-13 08:46:45 +0100

    Update list of snaps (#52)
    Co-authored-by: Alexandre Lissy <lissyx@lissyx.dyndns.org>

--- firefox-snap/script.sh
+++ firefox-snap/script.sh
@@ -8,10 +8,12 @@ export DEBUGINFOD_URLS="https://debuginfod.ubuntu.com/"
 process_snap "firefox-snap-stable" "firefox" "mozilla-snaps"
 process_snap "firefox-snap-stable-chemspill" "firefox" "mozilla-snaps"
 process_snap "firefox-snap-esr" "firefox" "mozilla-snaps"
+process_snap "firefox-snap-esr-128" "firefox" "mozilla-snaps"
 process_snap "firefox-snap-esr-chemspill" "firefox" "mozilla-snaps"
 process_snap "firefox-snap-beta" "firefox" "mozilla-snaps"
-process_snap "firefox-snap-core22" "firefox" "mozilla-snaps"
 process_snap "firefox-snap-nightly" "firefox" "mozilla-snaps"
+process_snap "firefox-snap-stable-core24" "firefox" "mozilla-snaps"
+process_snap "firefox-snap-beta-core24" "firefox" "mozilla-snaps"
 
 create_symbols_archive
 
