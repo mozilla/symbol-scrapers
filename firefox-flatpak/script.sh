67766674f2a8abd6f758f4edbf0062e0741389ed
lissyx <1645737+lissyx@users.noreply.github.com>
2025-02-18 21:03:12 +0100

    Fix Flatpak extraction

--- firefox-flatpak/script.sh
+++ firefox-flatpak/script.sh
@@ -3,13 +3,13 @@
 . $(dirname $0)/../common.sh
 . $(dirname $0)/../flatpak.sh
 
-process_flatpak \
-	"org.mozilla.firefox/x86_64/stable" \
-	"org.freedesktop.Platform.GL.Debug.default/x86_64/22.08" \
+process_flatpak                                                        \
+	"org.mozilla.firefox/x86_64/stable"                            \
+	"org.freedesktop.Platform.GL.Debug.default/x86_64/22.08"       \
 	"org.freedesktop.Platform.GL.Debug.default/x86_64/22.08-extra" \
-	"org.freedesktop.Platform.GL.Debug.default/x86_64/23.08" \
-	"org.freedesktop.Platform.GL.Debug.default/x86_64/23.08-extra"
-	"org.freedesktop.Platform.GL.Debug.default/x86_64/24.08" \
+	"org.freedesktop.Platform.GL.Debug.default/x86_64/23.08"       \
+	"org.freedesktop.Platform.GL.Debug.default/x86_64/23.08-extra" \
+	"org.freedesktop.Platform.GL.Debug.default/x86_64/24.08"       \
 	"org.freedesktop.Platform.GL.Debug.default/x86_64/24.08-extra"
 
 create_symbols_archive
